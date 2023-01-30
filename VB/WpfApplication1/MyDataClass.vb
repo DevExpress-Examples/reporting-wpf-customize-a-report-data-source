257dbedd42faf044bc5a2900493baeca989fe0a8
DevExpressExampleBot <fakeEmail>
2023-01-30 16:16:33 +0400

    autogenerate VB

--- VB/WpfApplication1/MyDataClass.vb
+++ VB/WpfApplication1/MyDataClass.vb
@@ -1,6 +1,9 @@
-﻿Namespace WpfApplication1
-	Public Class MyDataClass
-		Public Property ID() As Integer
-		Public Property Name() As String
-	End Class
+Namespace WpfApplication1
+
+    Public Class MyDataClass
+
+        Public Property ID As Integer
+
+        Public Property Name As String
+    End Class
 End Namespace
