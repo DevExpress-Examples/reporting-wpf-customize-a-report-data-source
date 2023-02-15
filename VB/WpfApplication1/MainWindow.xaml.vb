30e323b5e46a20330546df8480d08d4a2a4b8b57
DevExpressExampleBot <fakeEmail>
2023-02-15 15:52:11 +0400

    autogenerate VB

--- VB/WpfApplication1/MainWindow.xaml.vb
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -1,33 +1,33 @@
-﻿Imports System.ComponentModel
+Imports System.ComponentModel
 Imports System.Windows
 Imports DevExpress.DataAccess.ObjectBinding
-Imports DevExpress.Xpf.Reports.UserDesigner
 Imports DevExpress.XtraReports.UI
 
 Namespace WpfApplication1
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			designer.OpenDocument(New XtraReport1())
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub ReplaceDataSource_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			Dim newDataSource = New ObjectDataSource With {.DataSource = GetType(MyDataClass)}
-			Dim report As XtraReport = designer.ActiveDocument.Diagram.RootItem.XRObject
-			Dim oldDataSource = TryCast(report.DataSource, IComponent)
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.designer.OpenDocument(New XtraReport1())
+        End Sub
 
-			designer.ActiveDocument.MakeChanges(Sub(changes)
-				If oldDataSource IsNot Nothing Then
-					changes.RemoveItem(oldDataSource)
-				End If
-				changes.AddItem(newDataSource)
-				changes.SetProperty(report, Function(x) x.DataSource, newDataSource)
-			End Sub)
-		End Sub
-	End Class
+        Private Sub ReplaceDataSource_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Dim newDataSource = New ObjectDataSource With {.DataSource = GetType(MyDataClass)}
+            Dim report As XtraReport = Me.designer.ActiveDocument.Diagram.RootItem.XRObject
+            Dim oldDataSource = TryCast(report.DataSource, IComponent)
+            Me.designer.ActiveDocument.MakeChanges(Sub(changes)
+                If oldDataSource IsNot Nothing Then
+                    changes.RemoveItem(oldDataSource)
+                End If
+
+                changes.AddItem(newDataSource)
+                changes.SetProperty(report, Function(x) x.DataSource, newDataSource)
+            End Sub)
+        End Sub
+    End Class
 End Namespace
